6a59e615ece2cd6428285d38613d2c90c21b23a3
Runbin Shi <runbin.shi@inf.ethz.ch>
2022-12-16 15:33:20 +0100

    A smart table example with @coreui/vue-pro

--- vue_req/src/App.vue
+++ vue_req/src/App.vue
@@ -5,18 +5,18 @@ import GetReq from './components/GetReq.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+<!--    </div>-->
+<!--  </header>-->
 
-  <main>
-    <TheWelcome />
+<!--  <main>-->
+<!--    <TheWelcome />-->
     <GetReq />
-  </main>
+<!--  </main>-->
 </template>
 
 <style scoped>
--- vue_req/src/components/GetReq.vue
+++ vue_req/src/components/GetReq.vue
@@ -1,16 +1,35 @@
 <template>
-  <button @click="getReq">getReq</button>
-  <button @click="loadData">loadData</button>
-  <CTable :columns="columns" :items="items" />
+<!--  <button @click="getReq">getReq</button>-->
+<!--  <button @click="loadData">loadData</button>-->
+<!--  <CTable :columns="columns" :items="items" />-->
+
+  <CSmartTable
+      cleaner
+      clickableRows
+      :columns="columnsMultiSelect"
+      columnFilter
+      columnSorter
+      footer
+      :items="itemsMultiSelect"
+      itemsPerPageSelect
+      :itemsPerPage="10"
+      pagination
+      tableFilter
+  />
+
 </template>
 
 <script setup>
 import axios from 'axios';
-import '@coreui/coreui/dist/css/coreui.min.css'
+import '@coreui/coreui-pro/dist/css/coreui.css'
+import '@coreui/coreui-pro/dist/css/coreui.rtl.css'
+import '@coreui/coreui-pro/dist/css/coreui-grid.rtl.css'
+import '@coreui/coreui-pro/dist/css/coreui-utilities.css'
 import { CSpinner } from '@coreui/vue'
 import { CTableDataCell, CTableBody, CTable, CTableRow, CTableHeaderCell, CTableHead } from '@coreui/vue'
-// variable
-
+import { CSmartTable, CBadge, CButton, CCollapse, CCardBody  } from '@coreui/vue-pro'
+import { h } from 'vue'
+import { CMultiSelect } from '@coreui/vue-pro'
 </script>
 
 
@@ -24,34 +43,135 @@ async function getReq() {
 }
 
 export default {
-  data () {
+  data() {
     return {
-      columns: [ 'id', 'class', 'heading_1', 'heading_2'],
-      items: [
-        { id: 'Chetan Mohamed', class: '2018/02/01', heading_1: 'Admin', heading_2: 'hello' }
+      columnsMultiSelect: [
+        {
+          key: 'name',
+          _style: { width: '50%' },
+        },
+        {
+          key: 'role',
+          _style: { width: '50%' },
+          // filter: (values, onChange) => {
+          //   const unique = [...new Set(values)].sort()
+          //   return h(CMultiSelect, {
+          //     size: 'sm',
+          //     onChange: (selected) => {
+          //       const _selected = selected.map((element) => element.value)
+          //       onChange(function(item) {
+          //         return Array.isArray(_selected) && _selected.length
+          //             ? _selected.includes(item.toLowerCase())
+          //             : true
+          //       })
+          //     },
+          //     options: unique.map((element) => {
+          //       return {
+          //         value: element.toLowerCase(),
+          //         text: element,
+          //       }
+          //     })})
+          // },
+          // sorter: false,
+        },
+      ],
+      itemsMultiSelect: [
+        { id: 0, name: 'John Doe', role: 'Guest' },
+        {
+          id: 1,
+          name: 'Samppa Nori',
+          role: 'Member',
+        },
+        {
+          id: 2,
+          name: 'Estavan Lykos',
+          role: 'Staff',
+        },
+        { id: 3, name: 'Chetan Mohamed', role: 'Admin' },
+        {
+          id: 4,
+          name: 'Derick Maximinus',
+          role: 'Member',
+        },
+        { id: 5, name: 'Friderik Dávid', role: 'Staff' },
+        { id: 6, name: 'Yiorgos Avraamu', role: 'Member' },
+        {
+          id: 7,
+          name: 'Avram Tarasios',
+          role: 'Staff',
+        },
+        { id: 8, name: 'Quintin Ed', role: 'Admin' },
+        { id: 9, name: 'Enéas Kwadwo', role: 'Member' },
+        { id: 10, name: 'Agapetus Tadeáš', role: 'Staff' },
+        { id: 11, name: 'Carwyn Fachtna', role: 'Member' },
+        { id: 12, name: 'Nehemiah Tatius', role: 'Staff' },
+        { id: 13, name: 'Ebbe Gemariah', role: 'Admin' },
+        {
+          id: 14,
+          name: 'Eustorgios Amulius',
+          role: 'Member',
+        },
+        { id: 15, name: 'Leopold Gáspár', role: 'Staff' },
+        { id: 16, name: 'Pompeius René', role: 'Member' },
+        { id: 17, name: 'Paĉjo Jadon', role: 'Staff' },
+        {
+          id: 18,
+          name: 'Micheal Mercurius',
+          role: 'Admin',
+        },
+        {
+          id: 19,
+          name: 'Ganesha Dubhghall',
+          role: 'Member',
+        },
+        { id: 20, name: 'Hiroto Šimun', role: 'Staff' },
+        { id: 21, name: 'Vishnu Serghei', role: 'Member' },
+        { id: 22, name: 'Zbyněk Phoibos', role: 'Staff' },
+        {
+          id: 23,
+          name: 'Aulus Agmundr',
+          role: 'Member',
+        },
+        {
+          id: 42,
+          name: 'Ford Prefect',
+          role: 'Alien',
+        },
       ]
     }
-  },
-  
-  async created () {
-    const submodel = await getReq()
-    console.log(this.items[0]['id'])
-    this.items[0]['id'] = submodel.sub
-  },
-
-  methods: {
-    async loadData () {
-      const submodel = await getReq()
-      console.log(this.items[0]['id'])
-      this.items[0]['id'] = submodel.sub
-    }
-  },
-  watch: {
-    async updateData () {
-
-    }
-  },
+  }
 }
+
+// export default {
+//   data() {
+//     return {
+//       columns: ['id', 'class', 'heading_1', 'hello'],
+//       items: [
+//         {id: 'Chetan Mohamed', class: '2018/02/01', heading_1: 'Admin', heading_2: 'hello'}
+//       ]
+//     }
+//   },
+//
+//   async created() {
+//     const submodel = await getReq()
+//     console.log(this.items[0]['id'])
+//     this.items[0]['id'] = submodel.sub
+//   },
+//
+//   methods: {
+//     async loadData() {
+//       const submodel = await getReq()
+//       console.log(this.items[0]['id'])
+//       this.items[0]['id'] = submodel.sub
+//     }
+//   },
+//   watch: {
+//     async updateData() {
+//
+//     }
+//   },
+// }
+
 </script>
 
 <style scoped>
